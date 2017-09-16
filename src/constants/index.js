d66ebf07d2a7deee694e48dce48696ac45cac1a8
Phillipe Ramos <phillipeframos@gmail.com>
2017-09-16 16:10:00 -0300

    change boilerplate base

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+/**
+ * App Constants
+ *
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
+ */
+
+import AppConfig from './config';
+import ErrorMessages from './errors';
+import Firebase, { FirebaseRef } from './firebase';
+
+export { AppConfig, ErrorMessages, Firebase, FirebaseRef };
